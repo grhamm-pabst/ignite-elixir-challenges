0255fa4baf6f91b74f29620615bca71ef29e58f1
Grhamm Pabst <grhammpabst456@gmail.com>
2021-03-10 16:06:08 -0300

    ListLength challenge

---
+++ list_length/test/list_length_test.exs
@@ -0,0 +1,15 @@
+defmodule ListLengthTest do
+  use ExUnit.Case
+
+  describe "call/1" do
+    test "returns the list length" do
+      list = [1, 2, 3, 4]
+
+      response = ListLength.call(list)
+
+      expected_response = 4
+
+      assert response == expected_response
+    end
+  end
+end
